e11230cdacd56b363a86cada6920342b36fc4a4f
David Cole <David.Cole@kitware.com>
2005-11-04 19:35:04 +0000

    ENH: Merge changes from main tree into VTK-5-0 branch. (cvs -q up -j1.27 -j1.28 IO/vtkXMLDataReader.cxx)

--- IO/vtkXMLDataReader.cxx
+++ IO/vtkXMLDataReader.cxx
@@ -531,7 +531,7 @@ int vtkXMLDataReader::ReadArrayForCells(vtkXMLDataElement* da,
 
 //----------------------------------------------------------------------------
 int vtkXMLDataReader::ReadData(vtkXMLDataElement* da, void* data, int wordType,
-                               int startWord, int numWords)
+                               vtkIdType startWord, vtkIdType numWords)
 {
   // Skip real read if aborting.
   if(this->AbortExecute)
@@ -540,7 +540,7 @@ int vtkXMLDataReader::ReadData(vtkXMLDataElement* da, void* data, int wordType,
     }
   
   this->InReadData = 1;
-  unsigned long num = numWords;
+  vtkIdType num = numWords;
   int result;
   if(da->GetAttribute("offset"))
     {
