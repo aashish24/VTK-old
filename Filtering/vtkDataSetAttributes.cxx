87914376dfb020edf0e721cba6b072881dd97627
David Cole <David.Cole@kitware.com>
2006-04-19 14:22:12 +0000

    ENH: Merge changes from main tree into VTK-5-0 branch. (cvs -q up -j1.12 -j1.13 Filtering/vtkDataSetAttributes.cxx)

--- Filtering/vtkDataSetAttributes.cxx
+++ Filtering/vtkDataSetAttributes.cxx
@@ -41,6 +41,14 @@ const char vtkDataSetAttributes
   "TCoords",
   "Tensors" };
 
+const char vtkDataSetAttributes
+::LongAttributeNames[vtkDataSetAttributes::NUM_ATTRIBUTES][35] =
+{ "vtkDataSetAttributes::SCALARS",
+  "vtkDataSetAttributes::VECTORS",
+  "vtkDataSetAttributes::NORMALS",
+  "vtkDataSetAttributes::TCOORDS",
+  "vtkDataSetAttributes::TENSORS" };
+
 // Construct object with copying turned on for all data.
 vtkDataSetAttributes::vtkDataSetAttributes()
 {
@@ -1478,4 +1486,13 @@ const char* vtkDataSetAttributes::GetAttributeTypeAsString(int attributeType)
   return vtkDataSetAttributes::AttributeNames[attributeType];
 }
 
-
+//--------------------------------------------------------------------------
+const char* vtkDataSetAttributes::GetLongAttributeTypeAsString(int attributeType)
+{
+  if (attributeType < 0 || attributeType >= NUM_ATTRIBUTES)
+    {
+    vtkGenericWarningMacro("Bad attribute type.");
+    return NULL;
+    }
+  return vtkDataSetAttributes::LongAttributeNames[attributeType];
+}
