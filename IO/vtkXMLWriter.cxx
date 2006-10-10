1198917faa20ee09d79dec80d1a4a21c238bf952
David Cole <David.Cole@kitware.com>
2006-10-10 12:42:46 +0000

    ENH: Merge changes from main tree into VTK-5-0 branch. (cvs -q up -j1.67 -j1.68 IO/vtkXMLWriter.cxx)

--- IO/vtkXMLWriter.cxx
+++ IO/vtkXMLWriter.cxx
@@ -33,6 +33,8 @@
 #include "vtkOffsetsManagerArray.h"
 #undef  vtkOffsetsManager_DoNotInclude
 
+#include <vtksys/ios/sstream>
+
 #include <assert.h>
 #include <vtkstd/string>
 
@@ -1543,13 +1545,17 @@ void vtkXMLWriter::WriteDataArrayHeader(vtkDataArray* a, vtkIndent indent,
     {
     this->WriteStringAttribute("Name", alternateName);
     }
+  else if(const char* arrayName = a->GetName())
+    {
+    this->WriteStringAttribute("Name", arrayName);
+    }
   else
     {
-    const char* arrayName = a->GetName();
-    if(arrayName)
-      {
-      this->WriteStringAttribute("Name", arrayName);
-      }
+    // Generate a name for this array.
+    vtksys_ios::ostringstream name;
+    void* p = a;
+    name << "Array " << p;
+    this->WriteStringAttribute("Name", name.str().c_str());
     }
   if(a->GetNumberOfComponents() > 1)
     {
