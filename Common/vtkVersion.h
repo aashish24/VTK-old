9298e876490a71f647f85043516e6a20dbea4d97
David Cole <David.Cole@kitware.com>
2008-08-26 14:01:20 +0000

    ENH: Comment only changes for VTK-5-2-0-rc2.

--- Common/vtkVersion.h
+++ Common/vtkVersion.h
@@ -16,11 +16,12 @@
 // .SECTION Description
 // Holds methods for defining/determining the current vtk version
 // (major, minor, build).
-
+//
 // .SECTION Caveats
 // This file will change frequently to update the VTKSourceVersion which
 // timestamps a particular source release.
-
+//
+// Updated for cvs tag VTK-5-2-0-rc2
 
 #ifndef __vtkVersion_h
 #define __vtkVersion_h
