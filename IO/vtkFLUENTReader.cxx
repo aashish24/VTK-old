eb3dc4b830611bed83bc29bd55f83ef3478d2a62
Zack Galbreath <zack.galbreath@kitware.com>
2009-05-01 15:53:00 +0000

    ENH: Merge changes from main tree into VTK-5-4 branch. (cvs -q up -j1.23 -j1.25 IO/vtkFLUENTReader.cxx)

--- IO/vtkFLUENTReader.cxx
+++ IO/vtkFLUENTReader.cxx
@@ -181,6 +181,8 @@ vtkFLUENTReader::vtkFLUENTReader()
   this->FluentCaseFile = new ifstream;
   this->FluentDataFile = new ifstream;
 
+  this->NumberOfCells=0;
+  
   this->CellDataArraySelection = vtkDataArraySelection::New();
   this->SetDataByteOrderToLittleEndian();
 }
@@ -450,6 +452,7 @@ int vtkFLUENTReader::RequestInformation(
       this->VectorSubSectionIds->value.push_back(this->SubSectionIds->value[i]);
       }
     }
+  this->NumberOfCells = (int)this->Cells->value.size();
   return 1;
 }
 
@@ -744,8 +747,8 @@ int vtkFLUENTReader::GetDataChunk ()
     //file pointer to the "))" entry.
     char end[120];
     strcpy(end, "End of Binary Section   ");
-    strcat(end, index.c_str());
-    strcat(end, ")");
+    // strcat(end, index.c_str());
+    // strcat(end, ")");
     size_t len = strlen(end);
 
     // Load the data buffer enough to start comparing to the end vtkstd::string.
