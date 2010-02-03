0adf231fbfc9677eee41ac77868962d9342f5e7e
aashish chaudhary <aashish.chaudhary@kitware.com>
2010-02-03 13:33:19 -0500

    ENH: Moved the class and its definition to a separate header and source for code clearity and use.

--- Charts/vtkMark.cxx
+++ Charts/vtkMark.cxx
@@ -15,128 +15,16 @@
 
 #include "vtkMark.h"
 
-#include "vtkAbstractArray.h"
+//#include "vtkAbstractArray.h"
 #include "vtkBrush.h"
 #include "vtkContext2D.h"
 #include "vtkObjectFactory.h"
 #include "vtkPanelMark.h"
 #include "vtkPen.h"
-#include "vtkTable.h"
+//#include "vtkTable.h"
 
 #include "vtkBarMark.h"
 #include "vtkLineMark.h"
-
-//-----------------------------------------------------------------------------
-vtkIdType vtkDataElement::GetNumberOfChildren()
-{
-  switch (this->Type)
-    {
-    case TABLE:
-      if (this->Dimension == 0)
-        {
-        return this->Table->GetNumberOfRows();
-        }
-      else
-        {
-        return this->Table->GetNumberOfColumns();
-        }
-    case TABLE_ROW:
-      return this->Table->GetNumberOfColumns();
-    case ABSTRACT_ARRAY:
-      if (this->Dimension == 0)
-        {
-        return this->AbstractArray->GetNumberOfTuples();
-        }
-      else
-        {
-        return this->AbstractArray->GetNumberOfComponents();
-        }
-    case ABSTRACT_ARRAY_TUPLE:
-      return this->AbstractArray->GetNumberOfComponents();
-    case ABSTRACT_ARRAY_COMPONENT:
-      return this->AbstractArray->GetNumberOfTuples();
-    }
-  return 0;
-}
-
-//-----------------------------------------------------------------------------
-vtkDataElement vtkDataElement::GetChild(vtkIdType i)
-{
-  switch (this->Type)
-    {
-    case TABLE:
-      if (this->Dimension == 0)
-        {
-        return vtkDataElement(this->Table, i);
-        }
-      else
-        {
-        return vtkDataElement(this->Table->GetColumn(i));
-        }
-    case TABLE_ROW:
-      return vtkDataElement(this->Table->GetValue(this->Index, i));
-    case ABSTRACT_ARRAY:
-      if (this->Dimension == 0)
-        {
-        return vtkDataElement(this->AbstractArray, i, ABSTRACT_ARRAY_TUPLE);
-        }
-      else
-        {
-        return vtkDataElement(this->AbstractArray, i, ABSTRACT_ARRAY_COMPONENT);
-        }
-    case ABSTRACT_ARRAY_TUPLE:
-      return vtkDataElement(this->AbstractArray->GetVariantValue(this->Index*this->AbstractArray->GetNumberOfComponents() + i));
-    case ABSTRACT_ARRAY_COMPONENT:
-      return vtkDataElement(this->AbstractArray->GetVariantValue(i*this->AbstractArray->GetNumberOfComponents() + this->Index));
-    }
-  return vtkDataElement();
-}
-
-//-----------------------------------------------------------------------------
-vtkVariant vtkDataElement::GetValue(vtkIdType i)
-{
-  switch (this->Type)
-    {
-    case TABLE:
-      if (this->Dimension == 0)
-        {
-        return this->Table->GetValue(i, 0);
-        }
-      else
-        {
-        return this->Table->GetValue(0, i);
-        }
-    case TABLE_ROW:
-      return this->Table->GetValue(this->Index, i);
-    case ABSTRACT_ARRAY:
-      if (this->Dimension == 0)
-        {
-        return this->AbstractArray->GetVariantValue(i*this->AbstractArray->GetNumberOfComponents());
-        }
-      else
-        {
-        return this->AbstractArray->GetVariantValue(i);
-        }
-    case ABSTRACT_ARRAY_TUPLE:
-      return this->AbstractArray->GetVariantValue(this->Index*this->AbstractArray->GetNumberOfComponents() + i);
-    case ABSTRACT_ARRAY_COMPONENT:
-      return this->AbstractArray->GetVariantValue(i*this->AbstractArray->GetNumberOfComponents() + this->Index);
-    case SCALAR:
-      return this->Scalar;
-    }
-  return vtkVariant();
-}
-
-//-----------------------------------------------------------------------------
-vtkVariant vtkDataElement::GetValue(std::string str)
-{
-  switch (this->Type)
-    {
-    case TABLE_ROW:
-      return this->Table->GetValueByName(this->Index, str.c_str());
-    }
-  return vtkVariant();
-}
 
 //-----------------------------------------------------------------------------
 bool vtkDataElement::IsValid()
